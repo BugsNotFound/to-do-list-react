f25bcc281541b30b370889f201b17c499c1b30ac
Himansh Jain <Pctech4uproduction@gmail.com>
2020-07-08 11:56:56 +0530

    My first react project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './todoList/todoList';
+import AddToDo from './AddToDo/AddToDo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  
+  constructor(){
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+  
+  render(){
+    return(
+      <div>
+        <h1>To do List</h1>
+        <AddToDo addTodoFn={this.addTodo}></AddToDo>
+        <TodoList updateTodoFn={this.updateTodo} todos={this.state.todos}></TodoList>
+      </div>
+    );
+  }
+
+  componentDidMount = () => {
+    const todos = localStorage.getItem('todos');
+    if(todos){
+      const savedTodos = JSON.parse(todos);
+      this.setState({
+        todos: savedTodos
+      });
+    }
+    else{
+      console.log("No todos");
+    }
+  }
+
+  addTodo = async (todo) => {
+    await this.setState({todos: [...this.state.todos, {
+      text: todo,
+      completed: false
+    }]});
+    //this.state.todos = [...this.state.todos, todo];
+    //console.log(this.state.todos );
+    localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    console.log(localStorage.getItem('todos'));
+  }
+
+  updateTodo = async (todo) => {
+    const newTodos = this.state.todos.map(_todo => {
+      if(todo === _todo){
+        console.log("Came here...");
+        console.log(todo);
+        return {
+          text: todo.text,
+          completed: !todo.completed
+        }
+      }
+      else{
+        return _todo;
+      }
+    });
+
+    await this.setState({todos: newTodos});
+    //console.log(newTodos);
+    localStorage.setItem('todos', JSON.stringify(this.state.todos));
+  }
+
+   
+};
+
 
 export default App;
---
+++ src/todoList/todoList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TodoItem from '../todoItem/todoItem';
+
+class TodoList extends React.Component{
+    render(){
+        const {todos} = this.props; //destructuring...
+
+        return(
+            <div className="todoListContainer">
+                {
+                    todos.map((_todo, _index) => {
+                        return(
+                            <TodoItem updateTodoFn={this.updateTodo} key={_index} todo={_todo}></TodoItem>
+                        )
+                    })
+                }
+            </div>
+        );
+    }
+
+    updateTodo = (todo) => {
+        this.props.updateTodoFn(todo);
+    }
+};
+
+export default TodoList;
